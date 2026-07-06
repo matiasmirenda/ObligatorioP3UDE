387c93d02d8fc7d817d5bc05e98c00ffd0ce0bdc
Johana Madero <johanamadero3@gmail.com>
2026-07-05 22:05:18 -0300

    Modulo fecha

--- src/cpp/Fecha.cpp
+++ src/cpp/Fecha.cpp
@@ -1,15 +1,66 @@
 #include "../include/Fecha.h"
 
-Fecha CrearFecha(int dia, int mes, int anio)
+void CargarFecha(Fecha &f)
 {
-    Fecha f;
-    f.dia = dia;
-    f.mes = mes;
-    f.anio = anio;
-    return f;
+    printf("\n    Dia [Formato DD]: ");
+    scanf("%d", &f.dia);
+    printf("    Mes [Formato MM]: ");
+    scanf("%d", &f.mes);
+    printf("    Anio [Formato AAAA]: ");
+    scanf("%d", &f.anio);
 }
 
-bool EsBisiesto(Fecha f)
+void DarFecha(Fecha f)
 {
-    return (f.anio % 4 == 0);
+    printf("%02d / %02d / %04d", f.dia, f.mes, f.anio);
+}
+
+bool EsMismaFecha(Fecha f1, Fecha f2)
+{
+    bool igual = false;
+
+    if (f1.dia == f2.dia && f1.mes == f2.mes && f1.anio == f2.anio)
+        igual = true;
+
+    return igual;
+}
+
+bool EsFechaMayor(Fecha f1, Fecha f2)
+{
+    return (f1.anio > f2.anio) ||
+           (f1.anio == f2.anio && f1.mes > f2.mes) ||
+           (f1.anio == f2.anio && f1.mes == f2.mes && f1.dia > f2.dia);
+}
+
+bool EsFechaValida(Fecha f)
+{
+    bool fechaValida = false;
+    int diasMes;
+
+    if (f.mes >= 1 && f.mes <= 12)
+    {
+        switch (f.mes)
+        {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            diasMes = 30;
+            break;
+
+        case 2:
+            if (f.anio % 4 == 0)
+                diasMes = 29;
+            else
+                diasMes = 28;
+            break;
+
+        default:
+            diasMes = 31;
+        }
+
+        fechaValida = (f.dia >= 1 && f.dia <= diasMes);
+    }
+
+    return fechaValida;
 }
--- src/include/Fecha.h
+++ src/include/Fecha.h
@@ -1,5 +1,6 @@
 #ifndef FECHA_H
 #define FECHA_H
+#include "Boolean.h"
 
 struct Fecha
 {
@@ -8,8 +9,21 @@ struct Fecha
     int anio;
 };
 
-Fecha CrearFecha(int dia, int mes, int anio);
+// ‼️TODO: Cambiar bool por Boolean cuando este el módlo
 
-bool EsBisiesto(Fecha f);
+// Carga una fecha inresada por teclado
+void CargarFecha(Fecha &f);
+
+// Muestra por pantalla una fecha
+void DarFecha(Fecha f);
+
+// Devuelve TRUE si ambas fechas son iguales o FALSE en caso contrario
+bool EsMismaFecha(Fecha f1, Fecha f2);
+
+// Devuelve TRUE si la fecha f1 es mayor que la fecha f2 o FALSE en caso contrario
+bool EsFechaMayor(Fecha f1, Fecha f2);
+
+// Devuelve TRUE si la fecha ingresada es válida o FALSE en caso contrario
+bool EsFechaValida(Fecha f1);
 
 #endif
